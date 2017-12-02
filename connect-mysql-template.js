5854d5054bcd7c0b3ba665f5f427a115cdcb62e3
cxystras <cxystras@users.noreply.github.com>
2017-12-02 20:26:02 +0000

    Create connect-mysql-template.js

---
+++ connect-mysql-template.js
@@ -0,0 +1,60 @@
+var mysql      = require('mysql');
+var connection = mysql.createConnection({
+  host     : '',
+  user     : '',
+  password : '',
+  database : 'test'
+});
+
+connection.connect();
+connection.query('set autocommit=0',function(err,rows,fields){
+  if (!err)
+    console.log(rows);
+  else
+    console.log('Error setting autocommit off');
+});
+
+connection.query('show variables like "autocommit"',function(err,rows,fields){
+  if (!err)
+    console.log(rows);
+  else
+    console.log('Error showing autocommit status');
+});
+
+for (var i=0; i<10;i++){
+connection.query('show variables like "%ssl%"', function(err, rows, fields) {
+  if (!err)
+    console.log()+
+    console.log(rows)+
+    console.log();
+  else
+    console.log('Error while performing Query.');
+});
+};
+
+setTimeout(function() {console.log('Sleeping for 3 sec');},3000);
+
+connection.query('set autocommit=1',function(err,rows,fields){
+  if (!err)
+    console.log(rows);
+  else
+    console.log('Error setting autocommit on');
+});
+
+connection.query('show variables like "autocommit"',function(err,rows,fields){
+  if (!err)
+    console.log(rows);
+  else
+    console.log('Error showing autocommit status');
+});
+
+connection.query('show status like "%ssl%"', function(err,rows,fields) {
+  if (!err)
+    console.log()+
+    console.log(rows);
+   // console.log();
+  else
+    console.log('Error while performing Query 2.');
+});
+
+connection.end();
